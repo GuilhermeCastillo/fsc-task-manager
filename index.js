7858e79ccd7be8adbcb38bfb152a4555226fa84f
Guilheme Castilho <70145464+GuilhermeCastillo@users.noreply.github.com>
2025-03-03 00:54:03 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -7,12 +7,43 @@ const TaskModel = require("./src/models/task.model");
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
 app.get("/tasks", async (req, res) => {
     const tasks = await TaskModel.find({});
     res.status(200).send(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send("created");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const taskToDelete = await TaskModel.findById(taskId);
+
+        if (!taskToDelete) {
+            return res.status(500).send("Essa tarefa nao foi encontrada");
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(8000, () => console.log("listenin on port 8000"));
