5535bb42e2933316da98179a4f3588561aa02275
Guilheme Castilho <70145464+GuilhermeCastillo@users.noreply.github.com>
2025-03-03 15:22:05 -0300

    feat: move get task by id endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -3,13 +3,43 @@ const TaskModel = require("../models/task.model");
 class TaskController {
     constructor(req, res) {
         this.req = req;
-        this.req = res;
+        this.res = res;
     }
 
     async getTasks() {
         try {
             const tasks = await TaskModel.find({});
             this.res.status(200).send(tasks);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async getTaskById() {
+        try {
+            const taskId = this.req.params.id;
+
+            const task = await TaskModel.findById(taskId);
+
+            if (!task) {
+                return this.res
+                    .status(404)
+                    .send("essa tarefa não foi encontrada");
+            }
+
+            return this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async PostTasks() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+
+            await newTask.save();
+
+            this.res.status(201).send("created");
         } catch (error) {
             this.res.status(500).send(error.message);
         }
